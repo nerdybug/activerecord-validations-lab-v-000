19e8c5adbc89727aa4ca0ed4d884c9113f49d6f4
Meredith Reece <mardebugger@gmail.com>
2018-07-22 03:23:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@
 class ClickbaitCheck < ActiveRecord::Validator
   def validate(record)
     unless record.title.include? ("Won't Believe" || "Secret" || "Top [number]" || "Guess" )
+      post.errors[:title] << "Not clickbait-y enough"
     end
   end
 end
