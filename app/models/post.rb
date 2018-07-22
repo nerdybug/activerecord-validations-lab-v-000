5a66fd7a89d60b98e02ac648c08ac2103af88fdc
Meredith Reece <mardebugger@gmail.com>
2018-07-22 03:23:00 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,12 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates_with ClickbaitCheck
+end
 
-  def validate(post)
-    unless !post.title.include? ("Won't Believe" || "Secret" || "Top [number]" || "Guess" )
+class ClickbaitCheck < ActiveRecord::Validator
+  def validate(record)
+    unless record.title.include? ("Won't Believe" || "Secret" || "Top [number]" || "Guess" )
     end
   end
 end
