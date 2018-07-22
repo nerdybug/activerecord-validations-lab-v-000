3b6d67658be3839a88636bb899116100b89afa4f
Meredith Reece <mardebugger@gmail.com>
2018-07-22 03:25:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 
 class ClickbaitCheck < ActiveModel::Validator
   def validate(post)
-    unless post.title.include? ("Won't Believe" || "Secret" || "Top [number]" || "Guess" )
-      post.errors[:title] << "Not clickbait-y enough"
+    if post.title
+      unless post.title.include? ("Won't Believe" || "Secret" || "Top [number]" || "Guess" )
+        post.errors[:title] << "Not clickbait-y enough"
+      end
     end
   end
 end
